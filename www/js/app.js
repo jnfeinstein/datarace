c0e9bb88b4eccb61ed4a30b5009702de9f2c0c94
Joel Feinstein <joel@nyansa.com>
2015-05-16 19:54:24 -0700

    Remove everything not related to downloading

--- www/js/app.js
+++ www/js/app.js
@@ -8,27 +8,8 @@
 angular.module('starter', [
                'ionic',
                'starter.controllers',
-               'starter.services',
-               'ionic.service.core',
-               'auth0',
-               'angular-storage',
-               'angular-jwt',
-               'ngResource',
+               'ionic.service.core'
 ])
-
-.factory("SERVER_URL", ['$location',
-  function($location) {
-    var host = $location.host() || "datarace.net",
-        port = $location.port();
-
-    var url = "http://" + host;
-
-    if (port) {
-      url += ":" + port;
-    }
-
-    return url + "/";
-  }])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -44,26 +25,6 @@ angular.module('starter', [
   });
 })
 
-.run(function($rootScope, auth, store, jwtHelper, $location) {
-  // This events gets triggered on refresh or URL change
-  $rootScope.$on('$locationChangeStart', function() {
-    if (!auth.isAuthenticated) {
-      var token = store.get('token');
-      if (token) {
-        if (!jwtHelper.isTokenExpired(token)) {
-          auth.authenticate(store.get('profile'), token);
-        } else {
-          // Either show Login page or use the refresh token to get a new idToken
-          $location.path('/');
-        }
-      }
-    }
-  });
-
-  // This hooks all auth events to check everything as soon as the app starts
-  auth.hookEvents();
-})
-
 .config(['$ionicAppProvider', function($ionicAppProvider) {
   // Identify app
   $ionicAppProvider.identify({
@@ -74,130 +35,6 @@ angular.module('starter', [
   });
 }])
 
-.config(function (authProvider, $httpProvider, jwtInterceptorProvider) {
-  // ...
-
-  jwtInterceptorProvider.tokenGetter = function(store, jwtHelper, auth) {
-    var idToken = store.get('token');
-    var refreshToken = store.get('refreshToken');
-    // If no token return null
-    if (!idToken || !refreshToken) {
-      return null;
-    }
-    // If token is expired, get a new one
-    if (jwtHelper.isTokenExpired(idToken)) {
-      return auth.refreshIdToken(refreshToken).then(function(idToken) {
-        store.set('token', idToken);
-        return idToken;
-      });
-    } else {
-      return idToken;
-    }
-  };
-
-  $httpProvider.interceptors.push('jwtInterceptor');
-  // ...
-})
-
-.config(function($stateProvider, $urlRouterProvider, authProvider, $httpProvider, jwtInterceptorProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-  .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
-
-  // Each tab has its own nav history stack:
-  //
-  .state('login', {
-    url: '/login',
-    templateUrl: 'templates/login.html',
-    controller: 'LoginCtrl',
-  })
-
-  .state('tab.home', {
-    url: '/home',
-    views: {
-      'tab-home': {
-        templateUrl: 'templates/tab-home.html',
-        controller: 'HomeCtrl'
-      }
-    },
-    data: {
-      // This tells Auth0 that this state requires the user to be logged in.
-      // If the user isn't logged in and he tries to access this state
-      // he'll be redirected to the login page
-      requiresLogin: true
-    }
-  })
-
-  .state('tab.leaderboard', {
-      url: '/leaderboard',
-      views: {
-        'tab-leaderboard': {
-          templateUrl: 'templates/tab-leaderboard.html',
-          controller: 'LeaderboardCtrl'
-        }
-      },
-      data: {
-        // This tells Auth0 that this state requires the user to be logged in.
-        // If the user isn't logged in and he tries to access this state
-        // he'll be redirected to the login page
-        requiresLogin: true
-      }
-    })
-
-  .state('tab.challenges', {
-    url: '/challenges',
-    views: {
-      'tab-challenges': {
-        templateUrl: 'templates/tab-challenges.html',
-        controller: 'ChallengesCtrl'
-      }
-    },
-    data: {
-      // This tells Auth0 that this state requires the user to be logged in.
-      // If the user isn't logged in and he tries to access this state
-      // he'll be redirected to the login page
-      requiresLogin: true
-    }
-  })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    },
-    data: {
-      // This tells Auth0 that this state requires the user to be logged in.
-      // If the user isn't logged in and he tries to access this state
-      // he'll be redirected to the login page
-      requiresLogin: true
-    }
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/home');
-
-  authProvider.init({
-    domain: 'datarace.auth0.com',
-    clientID: 'anDND1WDueNYUpAyiwp4JN5sJVfZGsfC',
-    //callbackURL: location.href,
-    loginState: 'login'
-  });
-
-})
-
 .constant("formatSizeUnits", function formatSizeUnits(bytes){
   if      (bytes>=1000000000) {bytes=(bytes/1000000000).toFixed(2)+' GB';}
   else if (bytes>=1000000)    {bytes=(bytes/1000000).toFixed(2)+' MB';}
@@ -206,16 +43,6 @@ angular.module('starter', [
   else if (bytes==1)          {bytes=bytes+' byte';}
   else                        {bytes='0 byte';}
   return bytes;
-})
-
-.constant("trimPicture", function(picture) {
-  if (!picture) return picture;
-  var match = picture.match(/^([^?]+)\?/);
-  if (match) {
-    return match[1];
-  } else {
-    return picture;
-  }
 })
 
 ;
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,31 +5,7 @@ var files = {
 
 angular.module('starter.controllers', [])
 
-.controller('LoginCtrl', function LoginCtrl(store, $scope, $location, auth, $ionicLoading) {
-  $scope.login = function() {
-    auth.signin({
-      authParams: {
-        scope: 'openid offline_access',
-        device: 'Mobile device'
-      }
-    }, function(profile, token, accessToken, state, refreshToken) {
-      // Success callback
-      store.set('profile', profile);
-      store.set('token', token);
-      store.set('refreshToken', refreshToken);
-      $location.path('/');
-    }, function() {
-      // Error callback
-    });
-  };
-
-  $scope.login();
-})
-
-.controller('HomeCtrl', function($scope, $http, $q, $interval, Invites, SERVER_URL, formatSizeUnits) {
-  var bytesToReport, reportInterval;
-
-  $scope.invites = Invites.query();
+.controller('HomeCtrl', function($scope, $http, $q, formatSizeUnits) {
   $scope.downloading = false;
 
   $scope.toggleDownload = function() {
@@ -40,8 +16,6 @@ angular.module('starter.controllers', [])
       $scope.canceler.resolve();
       $scope.xhr = null;
       $scope.canceler = null;
-      $interval.cancel(reportInterval);
-      report();
     }
   };
 
@@ -50,17 +24,9 @@ angular.module('starter.controllers', [])
     $scope.total = 0;
     $scope.time = 0;
     $scope.average = 0;
-    bytesToReport = 0;
-    reportInterval = $interval(function() {
-      report();
-    }, 5000);
     download();
   };
 
-  function report() {
-    $http.post(SERVER_URL + "counters", {bytes: bytesToReport});
-    bytesToReport = 0;
-  };
 
   $scope.getAverage = function() {
     return [new Date().getTime(), $scope.average];
@@ -85,7 +51,6 @@ angular.module('starter.controllers', [])
     var endTime = new Date().getTime();
 
     var fileSize = files[file];
-    bytesToReport += fileSize;
     $scope.total = $scope.total + fileSize;
     $scope.humanTotal = formatSizeUnits($scope.total);
     $scope.time = $scope.time + (endTime - startTime);
@@ -94,134 +59,4 @@ angular.module('starter.controllers', [])
       download();
     }
   }
-})
-
-.controller('LeaderboardCtrl', function($scope, Users, formatSizeUnits, trimPicture) {
-  $scope.leaders = Users.query();
-
-  $scope.doRefresh = function() {
-    $scope.leaders = Users.query(function() {
-      $scope.$broadcast('scroll.refreshComplete');
-    });
-  };
-
-  $scope.hasUsage = function(leader) {
-    return leader.bytes && leader.bytes > 0;
-  };
-
-  $scope.formatSizeUnits = formatSizeUnits;
-  $scope.trimPicture = trimPicture;
-})
-
-.controller('ChallengesCtrl', function($scope, $ionicModal, $q, Challenges, Invites, Users, auth, trimPicture, Counters, formatSizeUnits) {
-  $scope.challenges = [];
-  $scope.invites = [];
-  Users.query().$promise.then(function(users) {
-    $scope.users = users.filter(function(user) {
-      return user._id != auth.profile.user_id.replace('auth0|', '');
-    });
-  });
-
-  $ionicModal.fromTemplateUrl('templates/new-challenge.html', function(modal) {
-    $scope.modal = modal;
-  }, {
-    scope: $scope,
-    animation: 'slide-in-up'
-  });
-
-  $scope.addChallenge = function(name) {
-    $scope.name = '';
-    Challenges.save({
-      name: name,
-      users: $scope.users.filter(function(user) {
-        return user.selected;
-      }).map(function(user) {
-        return user._id;
-      })
-    })
-      .$promise.then(function(challenge) {
-        $scope.users.forEach(function(user) {
-          user.selected = false;
-        });
-        doRefresh();
-        $scope.modal.hide();
-      });
-  }
-
-  $scope.leaveChallenge = function(challenge) {
-
-  }
-
-  $scope.acceptInvite = function(invite) {
-    //Challenges.remove(chat);
-    Invites.save({
-      id: invite._id,
-      accept: true
-    }).$promise.then(function() {
-      doRefresh();
-    })
-  };
-
-  $scope.ignoreInvite = function(invite) {
-    //Challenges.remove(chat);
-    Invites.save({
-      id: invite._id,
-      accept: false
-    }).$promise.then(function() {
-      doRefresh();
-    })
-  };
-
-  $scope.doRefresh = function() {
-    $q.all([ Challenges.query().$promise, Invites.query().$promise, Counters.query().$promise ])
-      .then(function(rez) {
-        $scope.challenges = rez[0];
-        $scope.invites = rez[1];
-
-        rez[2].forEach(function(counter) {
-          $scope.challenges
-          .filter(function(challenge) {
-            return challenge._id == counter.challenge;
-          })
-          .forEach(function(challenge) {
-            challenge.bytes = formatSizeUnits(counter.bytes);
-          })
-        });
-        $scope.$broadcast('scroll.refreshComplete');
-      });
-  };
-
-  $scope.trimPicture = trimPicture;
-
-  $scope.doRefresh();
-})
-
-.controller('AccountCtrl', function($scope, auth, store, $location, Users, $http, SERVER_URL, formatSizeUnits, trimPicture) {
-  $scope.settings = {
-    enableFriends: true
-  };
-
-  $scope.profile = auth.profile;
-
-  $scope.me = {};
-
-  $scope.logout = function() {
-    auth.signout();
-    store.remove('profile');
-    store.remove('token');
-    $location.path('/');
-  };
-
-  $scope.doRefresh = function() {
-    $http.get(SERVER_URL + "users/me").success(function(data) {
-      $scope.me = data;
-      $scope.me.humanBytes = formatSizeUnits($scope.me.bytes);
-      $scope.$broadcast('scroll.refreshComplete');
-    });
-  };
-
-  $scope.doRefresh();
-
-  $scope.trimPicture = trimPicture;
 });
-
